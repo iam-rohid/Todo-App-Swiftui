9a777dc7213cab3400111b50a4afb53d46004a40
Rohidul Islam <rohidul209@gmail.com>
2021-12-23 23:16:37 +0600

    Initial Commit

---
+++ todo_app/todo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_appApp.swift
+//  todo_app
+//
+//  Created by Rohid on 23/12/21.
+//
+
+import SwiftUI
+
+@main
+struct todo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
